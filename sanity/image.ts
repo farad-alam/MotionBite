dc31961899ac51907c72af44636a36749a9744ee
MD AHMODULLAH <contactme.ahmod@gmail.com>
2026-08-26 15:14:17 +0600

    chore: fix sanity image-url deprecation warning

--- sanity/image.ts
+++ sanity/image.ts
@@ -1,7 +1,7 @@
-import imageUrlBuilder from '@sanity/image-url'
+import { createImageUrlBuilder } from '@sanity/image-url'
 import { client } from './client'
 
-const builder = imageUrlBuilder(client)
+const builder = createImageUrlBuilder(client)
 
 export function urlFor(source: { asset: { _ref: string } }) {
   return builder.image(source)
